5b4fae3b1eb7f87f65e9bdab102829f5d4229394
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-04-01 12:38:43 +0900

    add to axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from "axios";
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL =
+'https://firestore.googleapis.com/v1/projects/vue-first-login/databases/(default)/documents/';
 new Vue({
   render: h => h(App),
 }).$mount('#app')
