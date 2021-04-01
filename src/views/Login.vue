ee42ae778f0cd7803bb10e49c1a7f1b7bb13ddb6
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-04-01 19:57:02 +0900

    add to Register.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,22 +13,32 @@
       type="password"
       v-model="password">
     <br><br>
-    <button @click="login">
-      送信
-    </button>
+    <button @click="login">送信</button>
   </div>
 </template>
 
 <script>
+import axios from '../axios-auth'
 export default {
   data(){
     return {
-      email:"",
-      password:"",
+      email:'',
+      password:'',
     };
   },
   methods: {
-    login(){}
+    login(){
+      axios.post('/accounts:signInWithPassword?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz-IBM'
+      ,{
+        email: this.email,
+        password: this.password,
+        returnSecureToken: true
+      }).then( response => {
+        console.log(response);
+      });
+      this.email = ''
+      this.password = ''
+    }
   }
 };
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -38,6 +38,8 @@ export default {
       }).then( response => {
         console.log(response);
       });
+      this.email = ''
+      this.password = ''
     }
   }
 };
