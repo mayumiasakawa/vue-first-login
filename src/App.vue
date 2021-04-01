8f9a7f0f462262cf3457e3ae8331a1f2f0f88aad
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-04-01 14:19:09 +0900

    add to template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム：</label>
+    <input
+      id="name"
+      type="text"
+      v-model="name">
+    <br><br>
+    <button @click="createComment">
+      コメントをサーバーに送る
+    </button>
+    <label for="comment">コメント：</label>
+    <textarea
+      id="comment"
+      v-model="comment"
+    ></textarea>
+    <h2>掲示板</h2>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      name:"",
+      returun:""
+    }
+  },
 }
 </script>
 
