7a220434712e2d5b58cbe7108e69eeb1e5a79b45
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-04-01 21:58:30 +0900

    add to save localStorage token

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from '../axios-auth'
+import axios from '../axios-auth';
 import router from '../router';
 import axiosRefresh from '../axios-refresh';
 
@@ -11,51 +11,97 @@ export default new Vuex.Store({
     idToken: null
   },
   getters: {
-    idToken: state=> state.idToken
+    idToken: state => state.idToken
   },
   mutations: {
-    updateIdToken(state,idToken){
+    updateIdToken(state, idToken) {
       state.idToken = idToken;
     }
   },
   actions: {
-    login({ commit, dispatch }, authData){
-      axios.post('/accounts:signInWithPassword?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz-IBM'
-      ,{
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      }).then( response => {
-        commit('updateIdToken', response.data.idToken);
+    async autoLogin({ commit, dispatch }) {
+      const idToken = localStorage.getItem('idToken');
+      if (!idToken) return;
+      const now = new Date();
+      const expiryTimeMs = localStorage.getItem('expiryTimeMs');
+      const isExpired = now.getTime() >= expiryTimeMs;
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (isExpired) {
+        await dispatch('refreshIdToken', refreshToken);
+      } else {
+        const expiresInMs = expiryTimeMs - now.getTime();
         setTimeout(() => {
-          dispatch('refreshIdToken', response.data.refreshToken);
-        }, response.data.expiresIn * 1000);
-        router.push('/');
-      });
-    }
-  },
-  refreshIdToken( { commit, dispatch }, refreshToken){
-    axiosRefresh.post('/token?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz',
+          dispatch('refreshIdToken', refreshToken);
+        }, expiresInMs);
+        commit('updateIdToken', idToken);
+      }
+    },
+    login({ dispatch }, authData) {
+      axios
+        .post(
+          '/accounts:signInWithPassword?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz-IBM',
           {
-            grant_type: 'refresh_token',
-            refresh_token: refreshToken
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
           }
-          ).then(response => {
-            commit('updateIdToken', response.id_token);
-            setTimeout(()=> {
-              dispatch('refreshIdToken', response.data.refresh_token);
-            },response.data.expires_in * 1000);
+        )
+        .then(response => {
+          dispatch('setAuthData', {
+            idToken: response.data.idToken,
+            expiresIn: response.data.expiresIn,
+            refreshToken: response.data.refreshToken
           });
-  },
-  register({ commit }, authData){
-    axios.post('/accounts:signUp?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz-IBM'
-    ,{
-      email: authData.email,
-      password: authData.password,
-      returnSecureToken: true
-    }).then( response => {
-      commit('updateIdToken', response.data.idToken);
-      router.push('/');
-    });
+          router.push('/');
+        });
+    },
+    logout({ commit }) {
+      commit('updateIdToken', null);
+      localStorage.removeItem('idToken');
+      localStorage.removeItem('expiryTimeMs');
+      localStorage.removeItem('refreshToken');
+      router.replace('/login');
+    },
+    async refreshIdToken({ dispatch }, refreshToken) {
+      await axiosRefresh
+        .post('/token?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz-IBM', {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken
+        })
+        .then(response => {
+          dispatch('setAuthData', {
+            idToken: response.data.id_token,
+            expiresIn: response.data.expires_in,
+            refreshToken: response.data.refresh_token
+          });
+        });
+    },
+    register({ dispatch }, authData) {
+      axios
+        .post('/accounts:signUp?key=AIzaSyAF_9eyfy1WLxGq3hkuvI2cEPCG4Vz-IBM', {
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        })
+        .then(response => {
+          dispatch('setAuthData', {
+            idToken: response.data.idToken,
+            expiresIn: response.data.expiresIn,
+            refreshToken: response.data.refreshToken
+          });
+          router.push('/');
+        });
+    },
+    setAuthData({ commit, dispatch }, authData) {
+      const now = new Date();
+      const expiryTimeMs = now.getTime() + authData.expiresIn * 1000;
+      commit('updateIdToken', authData.idToken);
+      localStorage.setItem('idToken', authData.idToken);
+      localStorage.setItem('expiryTimeMs', expiryTimeMs);
+      localStorage.setItem('refreshToken', authData.refreshToken);
+      setTimeout(() => {
+        dispatch('refreshIdToken', authData.refreshToken);
+      }, authData.expiresIn * 1000);
+    }
   }
 });
